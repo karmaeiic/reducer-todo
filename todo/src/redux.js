cfdeec612de835e80dad1aba00a776ff39c8135b
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-10-20 15:35:02 -0700

    yuppers

---
+++ todo/src/redux.js
@@ -0,0 +1,34 @@
+import { createStore } from 'redux';
+
+
+export const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+// Reducer
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, action.payload]
+      };
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, complete: !todo.complete }
+            : todo
+        )
+      };
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload)
+      };
+    default:
+      return state;
+  }
+}
